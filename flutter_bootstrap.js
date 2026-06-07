fa6549289ec44612ede68fa804adad1721b1441f
Jesus <jesusanchezc30@gmail.com>
2026-06-07 13:38:18 +0200

    Actualización v3 - icono y nombre app

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2627032140" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3706179716" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
