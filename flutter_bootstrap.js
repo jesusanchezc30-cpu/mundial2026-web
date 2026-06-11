20aeed704b5e1add64b5715fc74fbdb6d0a918ca
Jesus <jesusanchezc30@gmail.com>
2026-06-11 20:42:52 +0200

    GoatCounter analytics + título Guía Mundial 2026

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3706179716" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3344238414" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
